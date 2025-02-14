5b68c4d90bd539e99543b6b5e17c282eab8250a0
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-02-14 17:38:17 +0530

    style login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,35 +1,41 @@
 import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import styles from "./Login.module.css"
 
 function Login() {
   const [handleSubmit] = useOutletContext();
   const [username, setUsername] = useState("");
 
   return (
-    <div>
-      <form
-        action=""
-        onSubmit={(event) => {
-          event.preventDefault();
-          handleSubmit(username);
-        }}
-      >
-        <label htmlFor="username">
-          Username :
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            name="username"
-            required
-          />
-        </label>
+    <div className={styles["login-container"]}>
+      <h1 className={styles.title}>BlazingCart</h1>
 
-        <button type="submit">Log in as User</button>
-      </form>
-
-      <button type="button" onClick={() => handleSubmit("Guest")} >Log in as Guest</button>
+      <div>
+        <form
+          action=""
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleSubmit(username);
+          }}
+          className={styles["login__form"]}
+        >
+          <label htmlFor="username" className={styles["form__label"]}>
+            Username :
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              name="username"
+              required
+            />
+          </label>
+          <button type="submit">Log in as User</button>
+        </form>
+        <button type="button" onClick={() => handleSubmit("Guest")}>
+          Log in as Guest
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/login/Login.test.jsx
+++ src/components/login/Login.test.jsx
@@ -4,8 +4,6 @@ import Login from "./Login.jsx";
 import { createMemoryRouter, Outlet, RouterProvider } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
-
-
 function testSetup() {
   const handleSubmit = vi.fn();
 
@@ -48,10 +46,12 @@ describe("Login", () => {
     const { router } = testSetup();
     render(<RouterProvider router={router}></RouterProvider>);
 
+    const loginTitle = screen.getByRole("heading");
     const userNameInput = screen.getByLabelText("Username :");
     const [loginAsUserButton, loginAGuestButton] =
       screen.getAllByRole("button");
 
+    expect(loginTitle.textContent).toMatch("BlazingCart");
     expect(userNameInput).toBeInTheDocument();
     expect(loginAsUserButton.textContent).toMatch("Log in as User");
     expect(loginAGuestButton.textContent).toMatch("Log in as Guest");
@@ -67,7 +67,7 @@ describe("Login", () => {
     await user.type(userNameInput, "Ganesh");
     await user.click(loginAsUserButton);
 
-    expect(handleSubmit).toHaveBeenCalledWith("Ganesh")    
+    expect(handleSubmit).toHaveBeenCalledWith("Ganesh");
   });
 
   it("calls the submit handler with Guest", async () => {
@@ -79,5 +79,5 @@ describe("Login", () => {
     await user.click(loginAsGuestButton);
 
     expect(handleSubmit).toHaveBeenCalledWith("Guest");
-  })
+  });
 });
